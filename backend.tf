baf606a8ab04f6c6f939e48053501e2da64e352b
rashika.sahu <rashika.sahu@nagarro.com>
2024-11-27 17:01:45 +0530

    changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = var.bucket_name
+    bucket         = "${var.bucket_name}"
     key            = "terraform.tfstate"
-    region         = var.aws_region
+    region         = "${var.aws_region}"
   }
 }
