9da16649ab95b3f1795aee9bfc16243a2acbf88f
Rashika Sahu <153533971+rashikasahu112@users.noreply.github.com>
2025-01-05 18:26:50 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 set -x # Print commands for debugging
 
+sudo apt install unzip -y
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 sudo apt update
 sudo apt install git
 
-# install docker on ubuntu 22
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -13,10 +17,3 @@ apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo systemctl enable docker
 sudo systemctl start docker
-
-# install aws cli
-sudo apt install unzip -y
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
