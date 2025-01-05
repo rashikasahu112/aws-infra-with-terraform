957904c51be20990f979d5d3507609095a30d704
Rashika Sahu <153533971+rashikasahu112@users.noreply.github.com>
2025-01-05 17:32:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-terraform-state-files-rashika"
+    bucket         = "s3-terraform-state-files-rashikasahu"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
   }
